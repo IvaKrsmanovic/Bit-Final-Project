cff35f58133ee7e2b32d35a40f472fbdd4b89660
Djanelej1 <jelena.dabic1@gmail.com>
2023-03-30 20:04:43 +0200

    4-5 update

--- bit-final/src/components/Home/Home.jsx
+++ bit-final/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { getData } from "../../service/service";
-import { CardItem } from "../CardItem/CardItem";
+import Card from "react-bootstrap/Card";
 import "./CandidateList.css";
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
   const initData = async () => {
     const dataResult = await getData("candidates");
     if (dataResult != null && dataResult.length > 0) {
-    setData(dataResult);
+      setData(dataResult);
     }
   };
 
@@ -21,34 +21,40 @@ const Home = () => {
   const setCandidatesUI = () => {
     return data.map((candidate) => {
       return (
-        <>
-        <div className="title-search">
-        <div className="col-4"><h2>Candidates</h2></div>
-       <div className="col-4"><div class="input-box">
-        <input type="text" class="form-control"/>
-        <i class="fa fa-search"></i>                    
-        </div></div>
-        
-        </div>
-        <hr style={{margin:"20px"}}></hr>
-    <div className="container"> 
-    
-    <div className="list">   
-    
-    
-        <CardItem key={candidate.id} name={candidate.name} email={candidate.email} avatar={candidate.avatar}  />
-        </div> </div>
-        </>
+        <Card className="card" key={candidate.id}>
+          <Card.Img
+            className="card-img"
+            src={candidate.avatar}
+            alt={candidate.avatar}
+          />
+
+          <Card.Body className="d-flex flex-column">
+            <Card.Title className="card-name">{candidate.name}</Card.Title>
+            <Card.Text className="card-text">{candidate.email}</Card.Text>
+          </Card.Body>
+        </Card>
       );
     });
   };
 
   return (
-  <>
-    {setCandidatesUI()}
-    </>
+    <div>
+      <div className="title-search border-bottom p-2">
+        <div className="col-4">
+          <h2>Candidates</h2>
+        </div>
+        <div className="col-4">
+          <div className="input-box">
+            <input type="text" className="form-control" />
+          </div>
+        </div>
+      </div>
+ 
+      <div className="container">
+        <div className="list">{setCandidatesUI()}</div>
+      </div>
+    </div>
   );
 };
 
 export default Home;
-
