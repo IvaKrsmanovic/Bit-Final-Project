dcb15b498952ba0e4398d5b9fa93bdccebd54298
danicamih <114804610+danicamih@users.noreply.github.com>
2023-04-04 20:15:23 +0200

    bpf-7

--- bit-final/src/App.js
+++ bit-final/src/App.js
@@ -4,18 +4,16 @@ import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import {Route, Routes, BrowserRouter } from "react-router-dom"
-import PopUp from "./components/PopUp/PopUp";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
-        {/* <Routes>
+        <Routes>
           <Route path="/" element={<Home />} exact />
-          <Route path="/candidates/:id" element={<ReportsPage />} />
-          
-        </Routes> */}
+          <Route path="/candidates/:id" element={<ReportsPage />} />    
+        </Routes>
         <PopUp/>
         <Footer />
       </BrowserRouter>
